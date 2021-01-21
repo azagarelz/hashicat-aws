941a9c1321882cc56db7d32d8444b59140f751e8
Andrea Zagarella <andrea.zagarella96@gmail.com>
2021-01-21 20:34:23 +0000

    Added s3 bucket module

---
+++ s3-bucket.tf
@@ -0,0 +1,12 @@
+module "s3-bucket" {
+  source  = "app.terraform.io/ANDREAZAGARELLA-training/s3-bucket/aws"
+  version = "1.15.0"
+  # insert required variables here
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+}
